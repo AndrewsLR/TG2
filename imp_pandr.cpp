70d9e4b870cdb0f3b616c054b0f958d3ab3710c6
AndrewsLR <alrodrigues@inf.ufrgs.br>
2024-12-02 20:12:36 -0300

    funçoes arvore separadas em header

--- imp_pandr.cpp
+++ imp_pandr.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <string>
-#include <stack>
 #include <queue>
 #include <list>
 #include <fstream>
+
+#include "arv_binaria.h"
 using namespace std;
 
+int precedencia(char op);
+void faz_postfix(stack<char> &postfix, string eq);												//faz postfix
+void monta_arv(node *ptr, stack<char> &postfix);												//monta a arvore
+void printLevelOrder(node *root);																//printa a arvore
+void pinta_arv(node *root);
 
-typedef struct node{
-
-    char tipo;
-    int cor = 4;					//0 = preto-entrada real, 1 = branco-pseudo entrada,2 = mistoP- preto no topo, 3 = mistoB - branco no topo, 4 = indefinido
-    struct node* esquerda = nullptr;
-    struct node* direita = nullptr;
-	
-}node;
 
 typedef struct transistor{
 	char tipo;
@@ -119,133 +115,6 @@ void quebra_portas(string eq)
 	return;
 }
 
-void faz_postfix(stack<char>& postfix, string eq)
-{
-    stack<char> op;
-    
-    for(char& c : eq)
-    {
-        if (isalnum(c))
-        {
-            postfix.push(c);
-        }
-        else if (c == '(')
-        {
-            op.push(c);
-        }
-        else if (c == ')')
-        {
-            while (!op.empty() && op.top() != '(')
-            {
-                postfix.push(op.top());
-                op.pop();
-            }
-            op.pop();
-        }
-        else if (c == '+' || c == '*' || c == '-' || c == '/')
-        {
-
-            while (!op.empty() && op.top() != '(' && precedencia(op.top()) >= precedencia(c))
-            {
-                postfix.push(op.top());
-                op.pop();
-            }
-            op.push(c);
-        }
-    }
-
-    while (!op.empty())
-    {
-        if (op.top() != '(')
-            postfix.push(op.top());
-        op.pop();
-    }
-}
-
-
-int precedencia(char op)
-{
-	if(op == '*')
-		return 2;
-	if(op == '+')
-		return 1;
-	return 0;
-}
-
-void monta_arv(node *ptr, stack<char>& postfix)
-{
-	/*cout<<" postfix : ";
-	while(!postfix.empty())
-	{
-		 cout<< postfix.top()<<",";
-		postfix.pop();
-	}
-	cout<<endl;
-	return;*/
-	if(postfix.top() == '+' || postfix.top() == '*')
-	{
-		ptr->tipo = postfix.top();
-		postfix.pop();
-		ptr->direita = new node;
-		monta_arv(ptr->direita,postfix);
-		ptr->esquerda = new node;
-		monta_arv(ptr->esquerda,postfix);
-		
-	}
-	else
-	{
-		ptr->tipo = postfix.top();
-		ptr->cor = 0;
-		postfix.pop();
-	}
-	
-	return;
-}
-
-void pinta_arv(node *root)
-{
-	//enquanto tiver filhos que sao operacoes, desce para aquele filho, se filhos forem ambos in, cria pseudo, pinta nodo, retorna
-	//ao retornar criar pseudo, verifica cor de filhos, roda algoritmo, se necessario flip, cascata flip para filhos mistos
-	// ao terminar percorre arvore da esquerda para a direita para obter ordem TALVEZ OUTRA FUNCAO
-	// na realidade sempre existe um pseudo, apenas compara cores dos filhos e decide cor, mudar cor caso necessario (flip)
-	if(root->direita->tipo == '*' || root->direita->tipo == '+')
-	{
-		pinta_arv(root->direita);
-	}
-	
-	if(root->esquerda->tipo == '+' || root->esquerda->tipo == '*')
-	{
-		pinta_arv(root->esquerda);
-	}
-	else
-	{
-		root->cor = 3; //mistob = branco no topo
-	}
-	if(root->esquerda->cor != 4 && root->direita->cor != 4)
-	{
-		if(root->esquerda->cor == root->direita->cor)
-		{
-			if(root->esquerda->cor == 0)
-			{
-				root->cor = 3; // mistob = branco no topo
-			}
-			else
-			{
-				if(root->direita->cor == 3)
-				{
-					root->direita->cor = 2;
-				}
-				
-				root->cor = 1;
-			}
-		}
-		else
-		{
-			root->cor = 3; //mistob = branco no topo
-		}
-	} 
-	return;
-}
 
 void retorna_ordem(node *root, stack<char> &ordem) 	//se porta :coloca quebra no stack (tipo da porta) se iniciar em branco e navega filhos esquerda direita
 {													//se in : coloca in no stack
