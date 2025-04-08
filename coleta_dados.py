d4bc3e2d49bb2005ada305b72060f39368a054d1
AndrewsLR <andrews.rodrigues@inf.ufrgs.br>
2025-04-08 09:29:26 -0300

    Scripts python

---
+++ coleta_dados.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Pega dados do arquivo saida
+data = pd.read_csv('saida.txt', sep=" ", header=None)
+data.columns = ["eq","gaps","lines"]
+
+gaps = data["gaps"].value_counts()
+print(gaps)
+lines = data["lines"].value_counts()
+print(lines)
+
+#print(data.query("lines == 0"))
+#equacao = data.loc[2].at["eq"]
+#count = sum([1 for c in equacao if c.isalpha()])
+
+#Adiciona coluna inputs no dataframe
+data["inputs"] = ""
+#Preenche coluna com numero de entradas
+for index, row in data.iterrows():
+    data.at[index,"inputs"] = sum([1 for c in row["eq"] if c.isalpha()])
+print(data)
+
+inputs = data["inputs"].value_counts()
+print(inputs)
+
+plt.figure(figsize=(8,6))
+plt.hist(data["gaps"], bins= range(0, 7))
+plt.title('Histogram of Gap Number from genlib')
+plt.xlabel('Number of Gaps')
+plt.ylabel('Number of Equations')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+for value in sorted(data["gaps"].unique()):  # Sorting ensures ordered histograms
+    subset = data[data["gaps"] == value]  # Filter rows where inputs == value
+    print(subset)
+    plt.figure(figsize=(8,6))
+    plt.hist(subset["inputs"], bins=range(1, 18), edgecolor="black")
+    plt.title(f'Histogram for Gaps = {value}')
+    plt.xlabel('Number of Inputs')
+    plt.ylabel('Number of Equations')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    counts, bins, _ = plt.hist(subset["inputs"], bins=range(1, 18), edgecolor="black")
+    for i in range(len(counts)):
+        plt.text(bins[i] + 0.5, counts[i] + 0.1, str(int(counts[i])), ha='right', va='bottom', fontsize=16, color='black')
+    plt.show()
---
+++ desenha.py
@@ -0,0 +1,72 @@
+import cv2 as cv 
+import numpy as np
+import pandas as pd
+eq = 0
+with open("Netlists.spice","r") as f, open("Nets.txt","r") as net_file:
+    #abre spice e ignora primeiros 3 linha(sem netlist)
+    for _ in range(3):
+        next(f)
+    linha = f.readline()
+    while(len(linha) != 0 and eq < 3):
+        eq+=1
+        #Baseado em como inicia a linha, pega a equacao, ou o netlist
+        if linha.startswith("A"):
+            _, _, _, equacao = linha.split()
+            print(equacao)
+
+        #Guarda netlist em um dataframe
+        linha = f.readline()
+        netlist = pd.DataFrame(columns=['Drain', 'Gate', 'Source','Bulk','Type'])
+        while linha and linha.startswith("M"):
+            _, drain, gate, source, bulk, tipo = linha.split()
+            #print(numero,drain,gate,source,bulk,tipo)
+            netlist.loc[len(netlist)] = [drain, gate, source, bulk, tipo]
+            linha = f.readline()
+
+        print(netlist)
+
+    #Cria uma imagem vazia
+        blank = np.zeros((1000,1000,3), dtype='uint8')
+        n = netlist.shape[0]//2+1           #Comprimento da celula + 1(número de terminais efetivos)
+
+    #Abre txt e pega linhas de roteamento
+        net = net_file.readline()
+        if(net.startswith("N") == 0):
+            net = net_file.readline()
+        while(net.startswith("N")):
+            print(net)
+            nome, tamanho, _, num_lin = net.split()
+            inicio = int(tamanho[0]) - 1
+            fim = int(tamanho[2]) - 1
+            print("INICIO:",inicio," FIM:",fim)
+            cv.line(blank,(inicio*blank.shape[1]//n,(blank.shape[0]//3 + 50) * int(num_lin)),(fim*blank.shape[1]//n,(blank.shape[0]//3 + 50) * int(num_lin)),(255,0,255),8)
+            cv.putText(blank,nome,(inicio*blank.shape[1]//n,(blank.shape[0]//3 + 50) * int(num_lin)), cv.FONT_HERSHEY_PLAIN, 1.0,(255,0,0))
+            net = net_file.readline()
+
+        x_p = 0
+        x_n = 0
+        #blank[:] = 0,255,0
+        #cv.imshow('Green',blank)
+        for index,row in netlist.iterrows():
+            if(row['Type'] == "pfet"):
+                cv.rectangle(blank,(x_p * blank.shape[1]//n,0), ((blank.shape[1]//n)*(x_p+1), blank.shape[0]//3), (0,255,0), thickness=-1) #desenha difusao pull up
+                cv.putText(blank,row['Drain'],(x_p * blank.shape[1]//n + 10,50), cv.FONT_HERSHEY_PLAIN, 1.0,(255,0,0))
+                cv.line(blank, ((blank.shape[1]//n)*(x_p+1),0),((blank.shape[1]//n)*(x_p+1),1000), (0,0,255),8)
+                x_p+=1
+                if(x_p == n - 1):
+                    cv.rectangle(blank,(x_p * blank.shape[1]//n,0), ((blank.shape[1]//n)*(x_p+1), blank.shape[0]//3), (0,255,0), thickness=-1) #desenha difusao pull up
+                cv.putText(blank,row['Source'],(x_p * blank.shape[1]//n + 10,50), cv.FONT_HERSHEY_PLAIN, 1.0,(255,0,0))
+
+            if(row['Type'] == "nfet"):
+                cv.rectangle(blank,(x_n * blank.shape[1]//n,(blank.shape[0]//3)*2), ((blank.shape[1]//n)*(x_n+1), (blank.shape[0]//3)*3), (0,255,0), thickness=-1) # desenha difucao pull down
+                cv.putText(blank,row['Drain'],(x_n * blank.shape[1]//n + 10,((blank.shape[0]//3)*2) + 50), cv.FONT_HERSHEY_PLAIN, 1.0, (255,0,0))
+                x_n+=1
+                if(x_n == n - 1):
+                    cv.rectangle(blank,(x_n * blank.shape[1]//n,(blank.shape[0]//3)*2), ((blank.shape[1]//n)*(x_n+1), (blank.shape[0]//3)*3), (0,255,0), thickness=-1) # desenha difucao pull down
+                cv.putText(blank,row['Source'],(x_n * blank.shape[1]//n + 10,((blank.shape[0]//3)*2) + 50), cv.FONT_HERSHEY_PLAIN, 1.0, (255,0,0))
+
+        filename = str(eq)+".jpg"
+        print(filename)
+        cv.imwrite(filename,blank)
+#cv.imshow('Blank',blank)
+#cv.waitKey(0)
