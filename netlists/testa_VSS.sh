56c741b4c3001fb3f70030e15a2e90fb380fb7ee
AndrewsLR <alrodrigues@inf.ufrgs.br>
2025-05-17 09:45:24 -0300

    Scripts atualizados

---
+++ netlists/testa_VSS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+> "nets_no_vss.txt"
+for file in *.cdl; do
+	[ -e "$file" ] || continue
+	
+	if awk '{ if ($2 == "VSS" || $4 == "VSS") { found=1; exit } } END { exit !found }' "$file"; then
+		echo "$file"
+	else
+		echo "$file" >> "nets_no_vss.txt"
+	fi
+done
