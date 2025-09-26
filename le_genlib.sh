4c22b84786e93b98eb2663a0e568563a520841f5
AndrewsLR <andrews.rodrigues@inf.ufrgs.br>
2025-09-26 10:26:00 -0300

    Create le_genlib.sh

---
+++ le_genlib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+col_index=4 
+exe="./imp_pandr.exe"
+
+awk "NR>=3 && NR%2==1 {print \$$col_index}" 44-6.genlib | while read -r word; do
+    # strip first 3 chars and last char
+    processed="${word:3}"                 # drop first 3 chars
+    processed="${processed::-1}"          # drop last char
+    "$exe" "$processed"
+done
